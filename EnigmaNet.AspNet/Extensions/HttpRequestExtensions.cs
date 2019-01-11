5880c2e8a76ac44e4598dd7dc9b29514a7ae3b85
shenjingdong <shenjingdong@gmail.com>
2019-01-11 17:31:04 +0800

    添加httpRequestExtensions

---
+++ EnigmaNet.AspNet/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnigmaNet.AspNet.Extensions
+{
+    public static class HttpRequestExtensions
+    {
+        public static string GetUserAgent(this HttpRequest httpRequest)
+        {
+            return httpRequest.Headers["User-Agent"];
+        }
+
+        public static bool IsMicroMessager(this HttpRequest httpRequest)
+        {
+            var userAgent = httpRequest.GetUserAgent();
+
+            return userAgent.Contains("MicroMessenger");
+        }
+    }
+}
