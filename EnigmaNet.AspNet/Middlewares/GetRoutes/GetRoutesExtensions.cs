f0edb4fe2dc5f29d3eff54b973f8bc289ce77f11
shenjingdong <553058879@qq.com>
2019-03-19 21:09:56 +0800

    添加路由获取中间件

---
+++ EnigmaNet.AspNet/Middlewares/GetRoutes/GetRoutesExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Internal;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EnigmaNet.AspNet.Middlewares.GetRoutes
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// https://stackoverflow.com/questions/38505184/asp-net-core-get-routedata-value-from-url
+    /// </remarks>
+    public static class GetRoutesExtensions
+    {
+        public static IApplicationBuilder UseGetRoutes(this IApplicationBuilder app, Action<IRouteBuilder> configureRoutes)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            var routes = new RouteBuilder(app)
+            {
+                DefaultHandler = app.ApplicationServices.GetRequiredService<MvcRouteHandler>(),
+            };
+
+            configureRoutes(routes);
+
+            routes.Routes.Insert(0, AttributeRouting.CreateAttributeMegaRoute(app.ApplicationServices));
+
+            var router = routes.Build();
+
+            return app.UseMiddleware<GetRoutesMiddleware>(router);
+        }
+    }
+}
---
+++ EnigmaNet.AspNet/Middlewares/GetRoutes/GetRoutesMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace EnigmaNet.AspNet.Middlewares.GetRoutes
+{
+    public class GetRoutesMiddleware
+    {
+        class RoutingFeature : IRoutingFeature
+        {
+            public RouteData RouteData { get; set; }
+        }
+
+        IRouter _router;
+        RequestDelegate _next;
+
+        public GetRoutesMiddleware(RequestDelegate next, IRouter router)
+        {
+            _next = next;
+            _router = router;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var context = new RouteContext(httpContext);
+
+            context.RouteData.Routers.Add(_router);
+
+            await _router.RouteAsync(context);
+
+            if (context.Handler != null)
+            {
+                httpContext.Features[typeof(IRoutingFeature)] = new RoutingFeature
+                {
+                    RouteData = context.RouteData
+                };
+            }
+
+            await _next(httpContext);
+        }
+    }
+}
