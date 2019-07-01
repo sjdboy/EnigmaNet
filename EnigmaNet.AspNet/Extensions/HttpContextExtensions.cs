3cd9cb9185b48cd08f1e4fec44462684aec94731
shenjingdong <shenjingdong@hotmail.com>
2019-07-01 17:32:27 +0800

    添加HttpContextExtensions,添加获取当前area的扩展方法

---
+++ EnigmaNet.AspNet/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace EnigmaNet.AspNet.Extensions
+{
+    public static class HttpContextExtensions
+    {
+        public static string GetAreaName(this HttpContext httpContext)
+        {
+            return httpContext.GetRouteData().Values["area"].ToString();
+        }
+    }
+}
