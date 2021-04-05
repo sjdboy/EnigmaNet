63fdf44449d2b30502749ad0d5dc8b3c44edf9cc
shenjingdong <shenjingdong@gmail.com>
2021-04-05 16:50:57 +0800

    修改api错误处理中间件注册的逻辑

--- EnigmaNet.AspNet/Middlewares/ApiExceptionHandler/ApiExceptionHandlerExtensions.cs
+++ EnigmaNet.AspNet/Middlewares/ApiExceptionHandler/ApiExceptionHandlerExtensions.cs
@@ -1,20 +1,44 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Primitives;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace EnigmaNet.AspNet.Middlewares.ApiExceptionHandler
 {
     public static class ApiExceptionHandlerExtensions
     {
-        public static IApplicationBuilder UseApiExceptionHandler(this IApplicationBuilder app)
+        public static IApplicationBuilder UseApiExceptionHandler(this IApplicationBuilder app, params string[] apiPathPrefixs)
         {
-            return app.UseMiddleware<ApiExceptionHandlerMiddleware>();
-        }
+            return app.MapWhen(
+                context =>
+                {
+                    var requestPath = context.Request.Path.ToString();
 
-        public static IApplicationBuilder UseApiExceptionHandler(this IApplicationBuilder app, ApiExceptionHandlerOptions options)
-        {
-            return app.UseMiddleware<ApiExceptionHandlerMiddleware>(Microsoft.Extensions.Options.Options.Create(options));
+                    if (apiPathPrefixs?.Length > 0)
+                    {
+                        if (apiPathPrefixs.Any(pathPrefix => requestPath.StartsWith(pathPrefix, StringComparison.OrdinalIgnoreCase)))
+                        {
+                            return true;
+                        }
+                    }
+
+                    if (context.Request.Headers.TryGetValue("X-Requested-With", out StringValues xRequestedWithValues) &&
+                      xRequestedWithValues.Count > 0 && "XMLHttpRequest" == xRequestedWithValues[0])
+                    {
+                        return true;
+                    }
+
+                    if (context.Request.Headers.TryGetValue("Accept", out StringValues acceptValues) &&
+                        acceptValues.Count > 0 && acceptValues.Any(m => m.Contains("application/json")))
+                    {
+                        return true;
+                    }
+
+                    return false;
+                },
+                app2 => app2.UseMiddleware<ApiExceptionHandlerMiddleware>());
         }
     }
 }
--- EnigmaNet.AspNet/Middlewares/ApiExceptionHandler/ApiExceptionHandlerMiddleware.cs
+++ EnigmaNet.AspNet/Middlewares/ApiExceptionHandler/ApiExceptionHandlerMiddleware.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.Extensions.Primitives;
 
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
@@ -24,140 +20,102 @@ namespace EnigmaNet.AspNet.Middlewares.ApiExceptionHandler
         const string JsonContentType = "application/json";
 
         RequestDelegate _next;
-        ApiExceptionHandlerOptions _options;
         ILogger _logger;
 
-        public ApiExceptionHandlerMiddleware(
-            RequestDelegate next,
-            ILoggerFactory loggerFactory,
-            IOptions<ApiExceptionHandlerOptions> options)
+        public ApiExceptionHandlerMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
         {
             _next = next;
             _logger = loggerFactory.CreateLogger<ApiExceptionHandlerMiddleware>();
-            _options = options.Value;
         }
 
         public async Task Invoke(HttpContext httpContext)
         {
             var path = httpContext.Request.Path.ToString();
 
-            _logger.LogDebug($"apiExceptionHandler invoke:path:{path}");
-
-            bool isApi;
-
-            if (_options.ApiPathPrefixs?.Count > 0 &&
-                _options.ApiPathPrefixs.Any(m => path.StartsWith(m, StringComparison.OrdinalIgnoreCase)))
-            {
-                isApi = true;
-            }
-            else if (httpContext.Request.Headers.TryGetValue("X-Requested-With", out StringValues xRequestedWithValues) &&
-                   xRequestedWithValues.Count > 0 && "XMLHttpRequest" == xRequestedWithValues[0])
-            {
-                isApi = true;
-            }
-            else if (httpContext.Request.Headers.TryGetValue("Accept", out StringValues acceptValues) &&
-                acceptValues.Count > 0 &&
-                acceptValues.Any(m => m.Contains("application/json")))
-            {
-                isApi = true;
-            }
-            else
-            {
-                isApi = false;
-            }
-
-            if (isApi)
-            {
-                try
-                {
-                    await _next(httpContext);
-                }
-                catch (Exception ex)
-                {
-                    Exception exception;
-                    if (ex is AggregateException)
-                    {
-                        exception = ((AggregateException)ex).InnerException;
-                    }
-                    else
-                    {
-                        exception = ex;
-                    }
-
-                    if (exception is BizException)
-                    {
-                        var biz = (BizException)exception;
-
-                        var messageString = JsonConvert.SerializeObject(
-                            new MessageModel
-                            {
-                                Code = biz.ErrorCode,
-                                Message = biz.Message
-                            },
-                            new JsonSerializerSettings
-                            {
-                                ContractResolver = new DefaultContractResolver
-                                {
-                                    NamingStrategy = new CamelCaseNamingStrategy()
-                                }
-                            });
-
-                        httpContext.Response.Clear();
-                        httpContext.Response.StatusCode = Convert.ToInt32(System.Net.HttpStatusCode.Conflict);
-                        httpContext.Response.ContentType = JsonContentType;
-                        await httpContext.Response.WriteAsync(messageString);
-                    }
-                    else if (exception is ArgumentException)
-                    {
-                        var arg = (ArgumentException)exception;
-
-                        _logger.LogWarning(arg, $"request info,path:{httpContext.Request.Path} queryString:{httpContext.Request.QueryString}");
-
-                        var messageString = JsonConvert.SerializeObject(new MessageModel
-                        {
-                            Message = arg.Message
-                        },
-                        new JsonSerializerSettings
-                        {
-                            ContractResolver = new DefaultContractResolver
-                            {
-                                NamingStrategy = new CamelCaseNamingStrategy()
-                            }
-                        });
-
-                        httpContext.Response.Clear();
-                        httpContext.Response.StatusCode = Convert.ToInt32(System.Net.HttpStatusCode.BadRequest);
-                        httpContext.Response.ContentType = JsonContentType;
-                        await httpContext.Response.WriteAsync(messageString);
-                    }
-                    else
-                    {
-                        _logger.LogWarning(exception, $"request info,path:{httpContext.Request.Path} queryString:{httpContext.Request.QueryString}");
-
-                        var messageString = JsonConvert.SerializeObject(new MessageModel
-                        {
-                            Message = exception.Message
-                        },
-                        new JsonSerializerSettings
-                        {
-                            ContractResolver = new DefaultContractResolver
-                            {
-                                NamingStrategy = new CamelCaseNamingStrategy()
-                            }
-                        });
-
-                        httpContext.Response.Clear();
-                        httpContext.Response.StatusCode = Convert.ToInt32(System.Net.HttpStatusCode.InternalServerError);
-                        httpContext.Response.ContentType = JsonContentType;
-                        await httpContext.Response.WriteAsync(messageString);
-                    }
-                }
-            }
-            else
+            try
             {
                 await _next(httpContext);
             }
-        }
+            catch (Exception ex)
+            {
+                Exception exception;
+                if (ex is AggregateException)
+                {
+                    exception = ((AggregateException)ex).InnerException;
+                }
+                else
+                {
+                    exception = ex;
+                }
 
+                if (exception is BizException)
+                {
+                    var biz = (BizException)exception;
+
+                    var messageString = JsonConvert.SerializeObject(
+                        new MessageModel
+                        {
+                            Code = biz.ErrorCode,
+                            Message = biz.Message
+                        },
+                        new JsonSerializerSettings
+                        {
+                            ContractResolver = new DefaultContractResolver
+                            {
+                                NamingStrategy = new CamelCaseNamingStrategy()
+                            }
+                        });
+
+                    httpContext.Response.Clear();
+                    httpContext.Response.StatusCode = Convert.ToInt32(System.Net.HttpStatusCode.Conflict);
+                    httpContext.Response.ContentType = JsonContentType;
+                    await httpContext.Response.WriteAsync(messageString);
+                }
+                else if (exception is ArgumentException)
+                {
+                    var arg = (ArgumentException)exception;
+
+                    _logger.LogWarning(arg, $"request info,path:{httpContext.Request.Path} queryString:{httpContext.Request.QueryString}");
+
+                    var messageString = JsonConvert.SerializeObject(new MessageModel
+                    {
+                        Message = arg.Message
+                    },
+                    new JsonSerializerSettings
+                    {
+                        ContractResolver = new DefaultContractResolver
+                        {
+                            NamingStrategy = new CamelCaseNamingStrategy()
+                        }
+                    });
+
+                    httpContext.Response.Clear();
+                    httpContext.Response.StatusCode = Convert.ToInt32(System.Net.HttpStatusCode.BadRequest);
+                    httpContext.Response.ContentType = JsonContentType;
+                    await httpContext.Response.WriteAsync(messageString);
+                }
+                else
+                {
+                    _logger.LogWarning(exception, $"request info,path:{httpContext.Request.Path} queryString:{httpContext.Request.QueryString}");
+
+                    var messageString = JsonConvert.SerializeObject(new MessageModel
+                    {
+                        Message = exception.Message
+                    },
+                    new JsonSerializerSettings
+                    {
+                        ContractResolver = new DefaultContractResolver
+                        {
+                            NamingStrategy = new CamelCaseNamingStrategy()
+                        }
+                    });
+
+                    httpContext.Response.Clear();
+                    httpContext.Response.StatusCode = Convert.ToInt32(System.Net.HttpStatusCode.InternalServerError);
+                    httpContext.Response.ContentType = JsonContentType;
+                    await httpContext.Response.WriteAsync(messageString);
+                }
+            }
+        }
     }
 }
