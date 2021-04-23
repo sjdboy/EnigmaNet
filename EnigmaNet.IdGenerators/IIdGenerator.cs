f43a2e45ed93a1d515b148aeb227a4e4e0ed2b22
shenjingdong <shenjingdong@gmail.com>
2021-04-23 17:57:55 +0800

    移除EnigmaNet.EF,EnigmaNet.EFEventStore,EnigmaNet.EFIdProduce 添加EnigmaNet.IdGenerators

---
+++ EnigmaNet.IdGenerators/IIdGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnigmaNet.IdGenerators
+{
+    public interface IIdGenerator<T>
+    {
+        Task<T> GenerateIdAsync();
+    }
+}
---
+++ EnigmaNet.IdGenerators/Impl/SnowflakeIdGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnigmaNet.IdGenerators.Impl
+{
+    public class SnowflakeIdGenerator : ILongIdGenerator
+    {
+        Snowflake.IdWorker _idWorker;
+
+        public SnowflakeIdGenerator(long workerId, long datacenterId)
+        {
+            _idWorker = new Snowflake.IdWorker(workerId, datacenterId);
+        }
+
+        public Task<long> GenerateIdAsync()
+        {
+            var id = _idWorker.NextId();
+
+            return Task.FromResult(id);
+        }
+    }
+}
