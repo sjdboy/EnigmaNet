8c513b5647ed9a84ad5cb749fb7c796771af2ec1
shenjingdong <shenjingdong@gmail.com>
2021-09-25 18:04:13 +0800

    抖音视频信息接口增加状态字段

---
+++ EnigmaNet.DouYinOpenApi/Models/Video/VideoStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnigmaNet.DouYinOpenApi.Models.Video
+{
+    public enum VideoStatus
+    {
+        Published = 1,
+        NoSuitable = 2,
+        UnderReview = 4
+    }
+}
