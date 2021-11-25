be04ed0c1083899abb4f68b78b44bc9de9ee2156
shenjingdong <shenjingdong@gmail.com>
2021-11-25 17:15:08 +0800

    添加抖音视频新状态码

--- EnigmaNet.DouYinOpenApi/Models/Video/VideoStatus.cs
+++ EnigmaNet.DouYinOpenApi/Models/Video/VideoStatus.cs
@@ -8,8 +8,29 @@ namespace EnigmaNet.DouYinOpenApi.Models.Video
 {
     public enum VideoStatus
     {
+        /// <summary>
+        /// 已发布
+        /// </summary>
         Published = 1,
+        /// <summary>
+        /// 不适宜公开
+        /// </summary>
         NoSuitable = 2,
-        UnderReview = 4
+        /// <summary>
+        /// 审核中
+        /// </summary>
+        UnderReview = 4,
+        /// <summary>
+        /// 公开视频
+        /// </summary>
+        Public = 5,
+        /// <summary>
+        /// 好友可见
+        /// </summary>
+        FriendVisible = 6,
+        /// <summary>
+        /// 私密视频
+        /// </summary>
+        Private = 7,
     }
 }
