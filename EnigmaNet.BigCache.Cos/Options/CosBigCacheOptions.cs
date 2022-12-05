b68453e31e2da8c7437e16baddda11a647fd0f35
shenjingdong <shenjingdong@gmail.com>
2022-12-05 22:22:42 +0800

    添加大数据缓存项目

---
+++ EnigmaNet.BigCache.Cos/Options/CosBigCacheOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnigmaNet.BigCache.Cos.Options
+{
+    public class CosBigCacheOptions
+    {
+        public string AppId { get; set; }
+        public string SecretId { get; set; }
+        public string SecretKey { get; set; }
+        public string Region { get; set; }
+        public string Bucket { get; set; }
+    }
+}
---
+++ EnigmaNet.BigCache/IBigCache.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace EnigmaNet.BigCache
+{
+    public interface IBigCache
+    {
+        Task<T> GetAsync<T>(string key);
+
+        Task RemoveAsync(string key);
+
+        Task SetAsync<T>(string key, T obj);
+    }
+}
